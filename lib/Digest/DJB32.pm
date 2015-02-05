1b2b8e71b00e97a3a63244e303ef57644176012b
Torsten Raudssus <torsten@raudss.us>
2015-02-05 04:28:12 +0100

    First release, looks good ;)

--- lib/Digest/DJB32.pm
+++ lib/Digest/DJB32.pm
@@ -1,5 +1,37 @@
+package Digest::DJB32;
+# ABSTRACT: Digest::DJB fixed to 32bit
+
 use strict;
 use warnings;
-package Digest::DJB32;
+
+require Exporter;
+require DynaLoader;
+
+our @ISA = qw(Exporter DynaLoader);
+
+our @EXPORT_OK = qw( djb );
+
+our @EXPORT    = qw( );
+
+bootstrap Digest::DJB32 $Digest::DJB32::VERSION;
+
+# Preloaded methods go here.
 
 1;
+
+=head1 SYNOPSIS
+
+  use Digest::DJB32 qw(djb32);
+  
+  my $hash = djb32("abc123");
+
+=head1 DESCRIPTION
+
+C<Digest::DJB32> is an implementation of D. J. Bernstein's hash which returns
+a 32-bit unsigned value for any variable-length input string.
+
+=head1 SEE ALSO
+
+L<Digest::DJB>
+
+=cut
